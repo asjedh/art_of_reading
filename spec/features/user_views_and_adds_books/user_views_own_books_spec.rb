a6b3649cfdbe21077be07be850464c662cd93486
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-07-03 19:46:24 -0400

    Add test for viewing books when user is not signed in. Tests passing

--- spec/features/user_views_and_adds_books/user_views_own_books_spec.rb
+++ spec/features/user_views_and_adds_books/user_views_own_books_spec.rb
@@ -11,4 +11,19 @@ feature "user can view their own books" do
       expect(page).to have_content(book.description)
     end
   end
+
+  it "redirects to sign in page if user is not signed in" do
+    user = FactoryGirl.create(:user)
+    books = FactoryGirl.create_list(:book, 5, user: user)
+
+    visit user_books_path(user.id)
+
+    books.each do |book|
+      expect(page).to_not have_content(book.title)
+      expect(page).to_not have_content(book.description)
+    end
+
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
+  end
+
 end
