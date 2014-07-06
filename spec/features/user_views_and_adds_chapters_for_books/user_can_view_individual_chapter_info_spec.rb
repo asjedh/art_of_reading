b4623c44ad3d97de57667afdd6470df1519055f5
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-07-05 21:30:42 -0400

    Add spec for redirecting to login page

--- spec/features/user_views_and_adds_chapters_for_books/user_can_view_individual_chapter_info_spec.rb
+++ spec/features/user_views_and_adds_chapters_for_books/user_can_view_individual_chapter_info_spec.rb
@@ -2,8 +2,7 @@ require "spec_helper"
 
 feature "user can view chapter for a chapter" do
 
-  it "user views chapter information" do
-
+  it "shows chapter information if user visits chapter show page" do
     chapter = FactoryGirl.create(:chapter)
 
     sign_in_as(chapter.book.user)
@@ -14,5 +13,18 @@ feature "user can view chapter for a chapter" do
     expect(page).to have_content(chapter.summary)
     expect(page).to have_content(chapter.reflection)
     expect(page).to have_content(chapter.key_concepts)
+  end
+
+   it "redirects to login if user is not signed in" do
+    chapter = FactoryGirl.create(:chapter)
+
+    visit chapter_path(chapter.id)
+
+    expect(page).to_not have_content(chapter.title)
+    expect(page).to_not have_content(chapter.tldr)
+    expect(page).to_not have_content(chapter.summary)
+    expect(page).to_not have_content(chapter.reflection)
+    expect(page).to_not have_content(chapter.key_concepts)
+    expect(page).to have_content("You need to sign in or sign up before continuing.")
   end
 end
