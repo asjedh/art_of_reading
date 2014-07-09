d1be84941c2a3c1ab92f362758f860f6cff86bf1
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-07-09 14:16:40 -0400

    Add methods for creating book with goodreads api

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -32,7 +32,17 @@ class BooksController < ApplicationController
   end
 
   def goodreads_search
-    client = Good
+    goodreads_client = Goodreads::Client.new
+    search = goodreads_client.search_books(params[:search])
+    @results_to_display_on_view = parse_goodreads_search(search)
+  end
+
+  def goodreads_create
+    goodreads_client = Goodreads::Client.new
+    goodreads_book = goodreads_client.book(params[:goodreads_book_id])
+    book = create_book_with_book_info(goodreads_book)
+
+    redirect_to book_path(book.id)
   end
 
   private
@@ -40,4 +50,44 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :year)
   end
+
+  #these methods are for when the goodreads search by id is used
+  def create_book_with_book_info(goodreads_book)
+    book_info_hash = get_book_info_hash(goodreads_book)
+    book = Book.new(book_info_hash)
+    book.user = current_user
+    book.authors = assign_authors_from_goodreads(goodreads_book)
+  end
+
+  def get_book_info_hash(goodreads_book)
+    {
+      title: goodreads_book.title,
+      goodreads_book_id: goodreads_book.id,
+      isbn_10: goodreads_book.isbn,
+      isbn_13: goodreads_book.isbn13
+    }
+  end
+
+  #these methods are for when the goodreads search by title, author, or isbn function is used
+  def parse_goodreads_search(search)
+    books = []
+
+    search.results.work.each do |book|
+      books << get_book_info_from_search(book)
+    end
+    books
+  end
+
+  def get_book_info_from_search(book)
+    {
+      title: book.best_book.title,
+      goodreads_book_id: book.best_book.id,
+      author: book.best_book.author.name,
+      goodreads_author_id: book.best_book.author.id,
+      img_url: book.best_book.image_url,
+      original_publication_year: book.original_publication_year
+    }
+  end
+
+
 end
