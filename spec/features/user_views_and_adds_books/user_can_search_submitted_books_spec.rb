c542f0a98d28824ec8bfe2f4e9ad8847a94a3f69
Asjed (Azi) Hussain <asjed.hussain@gmail.com>
2014-07-11 12:05:51 -0400

    Create search tests

--- spec/features/user_views_and_adds_books/user_can_search_submitted_books_spec.rb
+++ spec/features/user_views_and_adds_books/user_can_search_submitted_books_spec.rb
@@ -1,21 +1,55 @@
 require 'spec_helper'
 
-feature 'user is able to find search box and search' do
+feature 'user is able to find search box' do
 
-  it 'shows a list of relevant books when a user searches' do
+  it 'shows a list of relevant books when a user searches with title' do
 
-  beard = FactoryGirl.create(:beard, title: "tip")
-  matching_beard = FactoryGirl.create(:beard, title: "tippity")
-  not_matching_beard = FactoryGirl.create(:beard, title: "top")
+  book = FactoryGirl.create(:book, title: "tip")
+  matching_book = FactoryGirl.create(:book, title: "tippity")
+  not_matching_book = FactoryGirl.create(:book, title: "top")
 
-  visit "/"
-
-  fill_in "search", with: beard.title
+  visit root_path
+  fill_in "search", with: book.title
   click_on "Search"
 
-  expect(page).to have_content(beard.title)
-  expect(page).to have_content(matching_beard.title)
-  expect(page).to_not have_content(not_matching_beard.title)
-
+  expect(page).to have_content(book.title)
+  expect(page).to have_content(matching_book.title)
+  expect(page).to_not have_content(not_matching_book.title)
   end
+
+  it 'shows relevant books when a user searches with ISBN' do
+    book = FactoryGirl.create(:book, isbn_10: "1111111111")
+    matching_book = FactoryGirl.create(:book, isbn_13: "1111111111111")
+    not_matching_book = FactoryGirl.create(:book, isbn_10: "2222222222")
+    other_not_matching_book = FactoryGirl.create(:book, isbn_13: "2222222222222")
+
+    visit root_path
+    fill_in "search", with: book.isbn_10
+    choose "ISBN"
+    click_on "Search"
+
+    expect(page).to have_content(book.title)
+    expect(page).to have_content(matching_book.title)
+    expect(page).to_not have_content(not_matching_book.title)
+    expect(page).to_not have_content(other_not_matching_book.title)
+  end
+
+  it 'shows a list of relevant books when a user searches with ISBN' do
+    author = FactoryGirl.create(:author)
+    books = FactoryGirl.create_list(:book, 10)
+    books.each { |book| book.authors << author }
+    not_matching_book = FactoryGirl.create(:book)
+
+    visit root_path
+    fill_in "search", with: author.name
+    choose "Author"
+    click_on "Search"
+
+    books.each do |book|
+      expect(page).to have_content(book.title)
+    end
+
+    expect(page).to_not have_content(not_matching_book.title)
+  end
+
 end
